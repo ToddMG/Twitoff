321b11c7df550f5ecc6fd27b4696ac553d8cd794
ToddMG <munyontodd@gmail.com>
2019-11-13 10:20:16 -0800

    Improve HTML/CSS and Twitter functionality

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,7 +1,7 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
-
+from .twitter import add_or_update_user
 
 # Make app factory
 def create_app():
@@ -19,6 +19,24 @@ def create_app():
     def root():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
+
+    # Route for getting new users
+    @app.route('/user', methods=['POST']) # Uses form
+    @app.route('/user/<name>', methods=['GET']) # Requires parameter
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets= User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
 
     # Set route for resetting the Database
     @app.route('/reset')
